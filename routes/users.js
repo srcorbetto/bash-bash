a40397bf62c6e83b51ad7ebe2c588646c4ccdda8
Mike Corbett <srcorbetto@gmail.com>
2018-11-18 16:37:38 -0600

    Initial files

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  // res.send('respond with a resource');
+
+  res.json([
+    {
+      id: 1,
+      username: "mikeC"
+    },
+    {
+      id: 2,
+      username: "kaseyC"
+    }
+  ])
+});
+
+module.exports = router;
