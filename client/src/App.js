e8d543079f9de2fb19f103914615705a8924132e
Mike Corbett <srcorbetto@gmail.com>
2018-11-21 12:08:10 -0600

    moved auth methods to auth component

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,7 @@ import './App.css';
 import { db, auth } from './firebase';
 import AuthForm from './components/AuthForm/AuthForm';
 
-let email;
-let password;
-
 class App extends Component {
-  
-  state = { 
-    users: [],
-    email: null,
-    password: null
-  }
-
-  componentWillMount() {
-    auth.onAuthStateChanged(firebaseUser => {
-      if (firebaseUser) {
-          console.log(firebaseUser);
-      } else {
-          console.log('not logged in');
-      }
-    })
-  }
 
   componentDidMount() {
     fetch('/users')
@@ -31,46 +12,11 @@ class App extends Component {
       .then(users => this.setState({ users }));
   }
 
-  onSignIn() {
-    const promise = auth.signInWithEmailAndPassword(email, password);
-    promise.catch(e => console.log(e.message));
-  }
-
-  onSignUp() {
-    // const auth = firebase.auth();
-    // Promise
-    const promise = auth.createUserWithEmailAndPassword(email, password);
-    promise.catch(e => console.log(e.message));
-  }
-
-  onSignOut() {
-    auth.signOut();
-  }
-
-  onAddCredentials(e) {
-    if (e.target.type === 'email') {
-      email = e.target.value;
-      console.log(`Email: ${email}`)
-    } else if (e.target.type === 'password') {
-      password = e.target.value;
-      console.log(`Password: ${password}`)
-    }
-  }
-
   render() {
     return (
       <div className="App">
-        {/* <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )} */}
         <div className="container-fluid">
-          <AuthForm email={this.onAddCredentials}
-                    password={this.onAddCredentials}
-                    signUp={this.onSignUp}
-                    signIn={this.onSignIn}
-                    signOut={this.onSignOut}
-          />
+          <AuthForm />
         </div>
       </div>
     );
--- client/src/components/AuthForm/AuthForm.js
+++ client/src/components/AuthForm/AuthForm.js
@@ -1,36 +1,98 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import { db, auth } from '../../firebase';
 
 import './AuthForm.css';
 
-const AuthForm = props => {
-    return (
-        <div className="row">
-            <div className="col">
-                <div className="AuthForm">
-                    <input onChange={props.email}
-                           required
-                           type="email"
-                           placeholder="Email"
-                           id="txtEmail"
-                    />
-                    <input onChange={props.password}
-                           required type="password"
-                           placeholder="Password"
-                           id="txtPassword"
-                    />
-                    <button id="btnLogin"
-                            onClick={props.signIn}
-                            className="btn btn-action">Log in</button>
-                    <button onClick={props.signUp}
-                            id="btnSignUp"
-                            className="btn btn-action">Sign up</button>
-                    <button onClick={props.signOut}
-                            id="btnLogout"
-                            className="btn btn-action hide">Log out</button>
+class AuthForm extends Component {
+
+    state = {
+        email: null,
+        password: null
+    }
+
+    constructor(props) {
+        super(props);
+        this.onAddCredentials = this.onAddCredentials.bind(this);
+    } 
+
+    componentWillMount() {
+        auth.onAuthStateChanged(firebaseUser => {
+          if (firebaseUser) {
+              console.log(firebaseUser);
+              this.setState({
+                firebaseUser: firebaseUser.email
+              })
+          } else {
+              console.log('not logged in');
+          }
+        })
+      }
+
+      onSignIn = () => {
+        const emailFromState = this.state.email;
+        const passwordFromState = this.state.password;
+        const promise = auth.signInWithEmailAndPassword(emailFromState, passwordFromState);
+        promise.catch(e => console.log(e.message));
+      }
+    
+      onSignUp = () => {
+        const promise = auth.createUserWithEmailAndPassword(this.state.email, this.state.password);
+        promise.catch(e => console.log(e.message));
+      }
+    
+      onSignOut() {
+        auth.signOut();
+      }
+    
+      onAddCredentials(e) {
+          const userValue = e.target.value;
+        if (e.target.type === 'email') {
+            this.setState({
+                email: userValue
+            })
+        //   email = e.target.value;
+          console.log(`Email: ${this.state.email}`)
+        } else if (e.target.type === 'password') {
+            this.setState({
+                password: userValue
+            })
+        //   password = e.target.value;
+          console.log(`Password: ${this.state.password}`)
+        }
+      }
+
+      render() {
+          return (
+            <div className="row">
+                <div className="col">
+                    <div className="AuthForm">
+                        <input onChange={this.onAddCredentials}
+                            required
+                            type="email"
+                            placeholder="Email"
+                            id="txtEmail"
+                        />
+                        <input onChange={this.onAddCredentials}
+                            required type="password"
+                            placeholder="Password"
+                            id="txtPassword"
+                        />
+                        <button id="btnLogin"
+                                onClick={this.onSignIn}
+                                className="btn btn-action">Log in</button>
+                        <button onClick={this.onSignUp}
+                                id="btnSignUp"
+                                className="btn btn-action">Sign up</button>
+                        <button onClick={this.onSignOut}
+                                id="btnLogout"
+                                className="btn btn-action hide">Log out</button>
+                    </div>
                 </div>
-            </div>
-        </div>
-    )
+            </div>  
+          )
+      }
+
 }
 
 export default AuthForm;
